4d8e053a1eb0fbc5260ceb06616b227eac9b133d
hrsgmt <hrsgmt@gmail.com>
2026-02-09 06:56:09 +0530

    CRASH TEST

--- src/server.js
+++ src/server.js
@@ -34,3 +34,5 @@ app.get("/__proof__", async () => ({ build: "NEW_SERVER_OK" }));
 // rebuild Mon Feb  9 01:18:55 IST 2026
 
 app.get("/whoami", async () => ({ repo:"axiom-backend-strong", build:"NEW", time:Date.now() }));
+
+throw new Error("CRASH_TEST");
